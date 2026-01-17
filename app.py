6fc0e0433c8b694651d35eb58c704e5f64424db2
syedjaferk <learnwithjafer@gmail.com>
2026-01-17 21:35:41 +0530

    update: message for home page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask App Deployed on Railway! Hi All Hello World"
+    return "Flask App Deployed on Railway! Hi All Hello World, Welcome to Girikalan Magic Show"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
